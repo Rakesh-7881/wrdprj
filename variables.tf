2f9dd6cd0ea89f13e672f0d2a4a8409f77c5906a
Rakesh-7881 <rakesh7881kadam@gmail.com>
2025-09-17 16:47:03 +0530

    Create variables.tf
    new update

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "prefix" {
+  type    = string
+  default = "wp-demo"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
